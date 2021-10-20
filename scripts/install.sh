ad20eac6b75ac2379e7d23cb11d78ef60ca19cb3
kojim <ykojim@gmail.com>
2021-10-20 13:35:41 +0000

    init

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#cd /home/ubuntu/app/docker
-#docker build -t mycontainer:1.0
-#docker run -v /home/ubuntu/app/app mycontainer:1.0
+cd /home/ubuntu/app/docker
+docker build -t mycontainer:1.0 .
+docker run -v /home/ubuntu/app/app:/app mycontainer:1.0
