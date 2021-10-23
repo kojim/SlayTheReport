e0721022e4c9040eac35eedf2ce9cce96440b825
Ubuntu <ubuntu@ip-10-1-0-197.ap-northeast-1.compute.internal>
2021-10-23 13:01:26 +0000

    create pages

--- app/app.rb
+++ app/app.rb
@@ -3,9 +3,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'aws-sdk'
+
+# AWS.config(:credential_provider => AWS::Core::CredentialProviders::EC2Provider.new)
+client = Aws::S3::Client.new( :region => 'ap-northeast-1')
 
 get '/' do
   @text = "hello text3"
   erb :index
 end
+
+get '/mypage' do
+  @text = "hello text3"
+  erb :mypage
+end
+
+get '/mypage/edit/:runid' do
+  @text = "hello text3"
+  erb :edit
+end
+
+get '/view/:player_id/:runid' do
+  @text = "hello text3"
+  erb :view
+end
+
 
