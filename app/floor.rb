a2bf197ea2cbce4f055fabe9b853d1360dd12fff
Ubuntu <ubuntu@ip-10-1-0-197.ap-northeast-1.compute.internal>
2021-10-25 11:12:31 +0000

    update

--- app/floor.rb
+++ app/floor.rb
@@ -9,11 +9,12 @@ class Floor
   attr_accessor :gold_diff
   attr_accessor :max_hp
   attr_accessor :hp
-  attr_accessor :obtain_potions
-  attr_accessor :obtain_relics
-  attr_accessor :obtain_cards
+  attr_accessor :hp_diff
+  attr_accessor :player_choise
+  attr_accessor :obtain_objects
   attr_accessor :obtain_chosen_cards
-  attr_accessor :upgrade_card
+  attr_accessor :upgrade_cards
+  attr_accessor :remove_cards
   attr_accessor :player_comment
 
   def initialize
@@ -24,11 +25,12 @@ class Floor
     @gold_diff           = 99
     @max_hp              = -1
     @hp                  = -1
-    @obtain_potions      = []
-    @obtain_relics       = []
-    @obtain_cards        = []
+    @player_choise       = nil
+    @hp_diff             = 0
+    @obtain_objects      = []
     @obtain_chosen_cards = []
-    @upgrade_card        = []
+    @upgrade_cards       = []
+    @remove_cards        = []
     @player_comment      = 'no comment'
   end
 
@@ -50,7 +52,7 @@ class Run
     result.floors = []
     result.floors << Floor.new
     result.floors[0].image = 'neow'
-    result.floors[0].text = run_data['neow_bonus'] + '/' + run_data['neow_cost']
+    result.floors[0].player_choise = run_data['neow_bonus'] + '/' + run_data['neow_cost']
     run_data['path_per_floor'].each_with_index do |t,idx|
       f = Floor.new
       case t
@@ -69,22 +71,24 @@ class Run
     end
 
     run_data['damage_taken'].each do |e|
-      result.floors[e['floor'].to_i].image = 'enemy:' + e['enemies']
+      result.floors[e['floor'].to_i].image = e['enemies']
     end
 
     run_data['event_choices'].each do |e|
       result.floors[e['floor'].to_i].image = e['event_name']
-      result.floors[e['floor'].to_i].text = 'player choise:' + e['player_choice']
+      result.floors[e['floor'].to_i].player_choise = e['player_choice']
     end
 
     run_data['campfire_choices'].each do |e|
       result.floors[e['floor'].to_i].image = 'campfire'
-      result.floors[e['floor'].to_i].text = e['key']
-      result.floors[e['floor'].to_i].upgrade_card = e['data']
+      result.floors[e['floor'].to_i].player_choise = e['key']
+      if e['key'] == 'SMITH' then
+        result.floors[e['floor'].to_i].upgrade_cards << e['data']
+      end
     end
 
     run_data['relics_obtained'].each do |e|
-      result.floors[e['floor'].to_i].obtain_relics << e['key']
+      result.floors[e['floor'].to_i].obtain_objects << e['key']
     end
 
     pre_gold = 99
@@ -94,8 +98,11 @@ class Run
       pre_gold = e
     end
 
+    pre_hp = 0
     run_data['current_hp_per_floor'].each_with_index do |e, idx|
       result.floors[idx+1].hp = e
+      result.floors[idx+1].hp_diff = e - pre_hp
+      pre_hp = e
     end
 
     run_data['max_hp_per_floor'].each_with_index do |e, idx|
--- app/image.rb
+++ app/image.rb
@@ -1,9 +1,6 @@
 require 'json'
 
-def img(key, size=200)
-  if key.include? "+" then
-    key = key.gsub(/\+.*/, '')
-  end
+def img(raw_key, size=200)
   map = {
     # events
     'neow' =>  'misc/neow.png',
@@ -15,90 +12,15 @@ def img(key, size=200)
     'gold' =>  'misc/gold.png',
     # random events
     'Liars Game' => 'events/liarsGame.jpg',
-    # relics
-    'Ancient Tea Set' => 'relics/tea_set.png',
-    # Ironcrad cards
-    'Bash'               =>  'cards/ironcrad/bash.png',
-    'Anger'              =>  'cards/ironcrad/anger.png',
-    'Armaments'          =>  'cards/ironcrad/armaments.png',
-    'Body Slam'          =>  'cards/ironcrad/body_slam.png',
-    'Clash'              =>  'cards/ironcrad/clash.png',
-    'Cleave'             =>  'cards/ironcrad/cleave.png',
-    'Clothesline'        =>  'cards/ironcrad/clothesline.png',
-    'Flex'               =>  'cards/ironcrad/flex.png',
-    'Havoc'              =>  'cards/ironcrad/havoc.png',
-    'Headbutt'           =>  'cards/ironcrad/headbutt.png',
-    'Heavy Blade'        =>  'cards/ironcrad/heavy_blade.png',
-    'Iron Wave'          =>  'cards/ironcrad/iron_wave.png',
-    'Perfected Strike'   =>  'cards/ironcrad/perfected_strike.png',
-    'Pommel Strike'      =>  'cards/ironcrad/pommel_strike.png',
-    'Shrug It Off'       =>  'cards/ironcrad/shrug_it_off.png',
-    'Sword Boomerang'    =>  'cards/ironcrad/sword_boomerang.png',
-    'Thunderclap'        =>  'cards/ironcrad/thunderclap.png',
-    'True Grit'          =>  'cards/ironcrad/true_grit.png',
-    'Twin Strike'        =>  'cards/ironcrad/twin_strike.png',
-    'Warcry'             =>  'cards/ironcrad/warcry.png',
-    'Wild Strike'        =>  'cards/ironcrad/wild_strike.png',
-    'Battle Trance'      =>  'cards/ironcrad/battle_trance.png',
-    'Blood for Blood'    =>  'cards/ironcrad/blood_for_blood.png',
-    'Bloodletting'       =>  'cards/ironcrad/bloodletting.png',
-    'Burning Pact'       =>  'cards/ironcrad/burning_pact.png',
-    'Carnage'            =>  'cards/ironcrad/carnage.png',
-    'Combust'            =>  'cards/ironcrad/combust.png',
-    'Dark Embrace'       =>  'cards/ironcrad/dark_embrace.png',
-    'Disarm'             =>  'cards/ironcrad/disarm.png',
-    'Dropkick'           =>  'cards/ironcrad/dropkick.png',
-    'Dual Wield'         =>  'cards/ironcrad/dual_wield.png',
-    'Entrench'           =>  'cards/ironcrad/entrench.png',
-    'Evolve'             =>  'cards/ironcrad/evolve.png',
-    'Feel No Pain'       =>  'cards/ironcrad/feel_no_pain.png',
-    'Fire Breathing'     =>  'cards/ironcrad/fire_bleathing.png',
-    'Flame Barrier'      =>  'cards/ironcrad/flame_barrier.png',
-    'Ghostly Armor'      =>  'cards/ironcrad/ghostly_armor.png',
-    'Hemokinesis'        =>  'cards/ironcrad/hemokinesis.png',
-    'Infernal Blade'     =>  'cards/ironcrad/infernal_blade.png',
-    'Inflame'            =>  'cards/ironcrad/inflame.png',
-    'Intimidate'         =>  'cards/ironcrad/intimidate.png',
-    'Metallicize'        =>  'cards/ironcrad/metallicize.png',
-    'Power Through'      =>  'cards/ironcrad/power_through.png',
-    'Pummel'             =>  'cards/ironcrad/pummel.png',
-    'Rage'               =>  'cards/ironcrad/rage.png',
-    'Rampage'            =>  'cards/ironcrad/rampage.png',
-    'Reckless Charge'    =>  'cards/ironcrad/reckless_charge.png',
-    'Rupture'            =>  'cards/ironcrad/rapture.png',
-    'Searing Blow'       =>  'cards/ironcrad/searing_blow.png',
-    'Second Wind'        =>  'cards/ironcrad/second_wind.png',
-    'Seeing Red'         =>  'cards/ironcrad/seeing_red.png',
-    'Sentinel'           =>  'cards/ironcrad/sentinel.png',
-    'Sever Soul'         =>  'cards/ironcrad/sever_soul.png',
-    'Shockwave'          =>  'cards/ironcrad/shockwave.png',
-    'Spot Weakness'      =>  'cards/ironcrad/spot_weakness.png',
-    'Uppercut'           =>  'cards/ironcrad/uppercut.png',
-    'Whirlwind'          =>  'cards/ironcrad/whirlwind.png',
-    'Barricade'          =>  'cards/ironcrad/barricade.png',
-    'Berserk'            =>  'cards/ironcrad/berserk.png',
-    'Bludgeon'           =>  'cards/ironcrad/bludgeon.png',
-    'Brutality'          =>  'cards/ironcrad/brutality.png',
-    'Corruption'         =>  'cards/ironcrad/corruption.png',
-    'Demon Form'         =>  'cards/ironcrad/daemon_form.png',
-    'Double Tap'         =>  'cards/ironcrad/double_tap.png',
-    'Exhume'             =>  'cards/ironcrad/exhume.png',
-    'Feed'               =>  'cards/ironcrad/feed.png',
-    'Fiend Fire'         =>  'cards/ironcrad/fiend_fire.png',
-    'Immolate'           =>  'cards/ironcrad/immolate.png',
-    'Impervious'         =>  'cards/ironcrad/impervious.png',
-    'Juggernaut'         =>  'cards/ironcrad/juggernaut.png',
-    'Limit Break'        =>  'cards/ironcrad/limit_break.png',
-    'Offering'           =>  'cards/ironcrad/offering.png',
-    'Reaper'             =>  'cards/ironcrad/reaper.png',
   }
-  if map.key?(key) then
-    v = map[key]
-    if key.include? "+" then
-      v = v.gsub(/\./, '_p.')
-    end
-    return "<img src='https://slaythereport-images.s3.ap-northeast-1.amazonaws.com/#{v}' style='width:#{size}pt'></img>"
+  if map.key?(raw_key) then
+    v = map[raw_key]
   else
-    return 'no-image: ' + key
+    key = raw_key.gsub(/ /, '')
+    if key.include? "+" then
+      key = key.gsub(/\+.*/, 'Plus')
+    end
+    v = 'all/' + key + '.png'
   end
+  return "<img src='https://slaythereport-images.s3.ap-northeast-1.amazonaws.com/#{v}' alt='#{v}' style='width:#{size}pt'></img>"
 end
