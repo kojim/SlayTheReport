9c20663d06fb8976b1555dd35a2ba8c8c7098782
kojim <ykojim@gmail.com>
2024-02-10 04:03:36 +0000

    remove codepipeline

--- buildpipeline/2.app_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 947597759324.dkr.ecr.ap-northeast-1.amazonaws.com
-docker image prune -f
-docker pull 947597759324.dkr.ecr.ap-northeast-1.amazonaws.com/slaythereport_mountless
-cd /home/ubuntu/SlayTheReport/docker && docker-compose -f docker-compose-staging.yaml up -d sinatra
